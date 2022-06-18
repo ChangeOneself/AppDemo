50f7ca8173681c32c2478cd3c60f0bf7b64b348c
jiang <550311358@qq.com>
2022-06-18 14:06:17 +0800

    初始化SpringBoot项目

---
+++ src/main/java/cn/jwc/app/AppDemoApplication.java
@@ -0,0 +1,31 @@
+package cn.jwc.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: AppDemo
+ * @description: AppDemo启动类
+ * @author: Mr.Jiang
+ * @create: 2022-06-18 12:39
+ **/
+@SpringBootApplication
+@RestController
+@RequestMapping("/appdemotest")
+public class AppDemoApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppDemoApplication.class);
+    public static void main(String[] args) {
+        LOGGER.info("AppDemoApplication|start");
+        SpringApplication.run(AppDemoApplication.class, args);
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public void test(){
+        LOGGER.info("AppDemoApplication|test");
+    }
+}
